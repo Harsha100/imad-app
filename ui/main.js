d629907bc5174302ba4d559b63bf46dbd712acb4
Harsha100 <36588363+Harsha100@users.noreply.github.com>
2018-02-26 21:55:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 var img = document.getElementById('modi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginleft = marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
 };
